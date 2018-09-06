138f80205544779271a8259c38d20dcd029af3df
Brian Henson <brian.henson1@yahoo.com>
2018-09-05 20:53:26 -0700

    x

--- MinesweeperProject/run_everything.bat
+++ MinesweeperProject/run_everything.bat
@@ -1,11 +1,11 @@
 @ECHO OFF
 ECHO beginning minesweeper tests in all 6 modes
 PAUSE
-..\x64\Release\MinesweeperProject.exe -numgames 1000000 -field 30-16-99 -findz 0 -gmode 0 -scr -1
-..\x64\Release\MinesweeperProject.exe -numgames 1000000 -field 30-16-99 -findz 0 -gmode 1 -scr -1
-..\x64\Release\MinesweeperProject.exe -numgames 1000000 -field 30-16-99 -findz 0 -gmode 2 -scr -1
-..\x64\Release\MinesweeperProject.exe -numgames 1000000 -field 30-16-99 -findz 1 -gmode 0 -scr -1
-..\x64\Release\MinesweeperProject.exe -numgames 1000000 -field 30-16-99 -findz 1 -gmode 1 -scr -1
-..\x64\Release\MinesweeperProject.exe -numgames 1000000 -field 30-16-99 -findz 1 -gmode 2 -scr -1
+::..\x64\Release\MinesweeperProject.exe -numgames 1000000 -field 30-16-99 -findz 0 -gmode 0 -scr -1
+..\x64\Release\MinesweeperProject.exe -numgames 500000 -field 30-16-99 -findz 0 -gmode 1 -scr -1
+..\x64\Release\MinesweeperProject.exe -numgames 500000 -field 30-16-99 -findz 0 -gmode 2 -scr -1
+::..\x64\Release\MinesweeperProject.exe -numgames 1000000 -field 30-16-99 -findz 1 -gmode 0 -scr -1
+::..\x64\Release\MinesweeperProject.exe -numgames 500000 -field 30-16-99 -findz 1 -gmode 1 -scr -1
+::..\x64\Release\MinesweeperProject.exe -numgames 1000000 -field 30-16-99 -findz 1 -gmode 2 -scr -1
 ECHO EVERYTHING DONE!!
 PAUSE
